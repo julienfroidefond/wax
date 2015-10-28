658d9297584112fbd892cd36f67c163a45d0dbcc
Julien FROIDEFOND <jfroidefond@atixnet.fr>
2015-10-28 08:10:08 +0100

    avatar dans nav et bouton pour compréhension dans register

--- client/components/toolbar/toolbar.directive.ng.js
+++ client/components/toolbar/toolbar.directive.ng.js
@@ -1,10 +1,22 @@
 'use strict'
 
 angular.module('waxYeoAnguApp')
-.directive('toolbar', function() {
+.directive('toolbar', function($meteor, $filter) {
   return {
     restrict: 'AE',
     templateUrl: 'client/components/toolbar/toolbar.view.html',
-    replace: true
+    replace: true,
+    link: function($scope){
+
+        $scope.images = $meteor.collectionFS(Images, false, Images).subscribe('images');
+
+        $scope.getAvatarUrl = function(idToFind){
+            if(!idToFind) return "avatar.jpg";
+              if($filter('filter')($scope.images, {_id: idToFind}).length>0){
+                var url = $filter('filter')($scope.images, {_id: idToFind})[0].url();
+                return url;
+            }
+        }
+    }
   };
 });
