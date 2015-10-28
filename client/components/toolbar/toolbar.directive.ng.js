e6bf92fab948cb82b552dc571bd3681593d93bb5
Julien FROIDEFOND <jfroidefond@atixnet.fr>
2015-10-28 09:29:56 +0100

    revue de l'image du user connecté

--- client/components/toolbar/toolbar.directive.ng.js
+++ client/components/toolbar/toolbar.directive.ng.js
@@ -1,22 +1,16 @@
 'use strict'
 
 angular.module('waxYeoAnguApp')
-.directive('toolbar', function($meteor, $filter) {
+.directive('toolbar', function($meteor, $filter, $rootScope) {
   return {
     restrict: 'AE',
     templateUrl: 'client/components/toolbar/toolbar.view.html',
     replace: true,
     link: function($scope){
-
-        $scope.images = $meteor.collectionFS(Images, false, Images).subscribe('images');
-
-        $scope.getAvatarUrl = function(idToFind){
-            if(!idToFind) return "avatar.jpg";
-              if($filter('filter')($scope.images, {_id: idToFind}).length>0){
-                var url = $filter('filter')($scope.images, {_id: idToFind})[0].url();
-                return url;
-            }
-        }
+        var avatarId = $rootScope.currentUser.profile.avatar;
+        $scope.avatar = $meteor.collectionFS(function(){
+            return Images.find({_id : avatarId});
+        }, false).subscribe('images');
     }
   };
 });
