fa19b4d34590298cefc22d75df700b9a4ebdd364
Julien FROIDEFOND <jfroidefond@atixnet.fr>
2015-10-29 08:02:06 +0100

    user list dans about pour info

--- client/about/about.controller.ng.js
+++ client/about/about.controller.ng.js
@@ -1,7 +1,19 @@
 'use strict';
 
 angular.module('waxYeoAnguApp')
-.controller('AboutCtrl', function($scope) {
+.controller('AboutCtrl', function($scope, $meteor, $filter) {
   $scope.viewName = 'About';
   $scope.pageClass= "about-page";
+
+  $scope.images = $meteor.collectionFS(Images, false, Images).subscribe('images');
+
+  $scope.users = $meteor.collection(Meteor.users, false).subscribe('users');
+
+  $scope.getImageUrl = function(idToFind) {
+      if(!idToFind) return "avatar.jpg";
+        if($filter('filter')($scope.images, {_id: idToFind}).length>0){
+          var url = $filter('filter')($scope.images, {_id: idToFind})[0].url();
+          return url;
+      }
+  };
 });
--- client/components/toolbar/toolbar.directive.ng.js
+++ client/components/toolbar/toolbar.directive.ng.js
@@ -7,10 +7,12 @@ angular.module('waxYeoAnguApp')
     templateUrl: 'client/components/toolbar/toolbar.view.html',
     replace: true,
     link: function($scope){
-        var avatarId = $rootScope.currentUser.profile.avatar;
-        $scope.avatar = $meteor.collectionFS(function(){
-            return Images.find({_id : avatarId});
-        }, false).subscribe('images');
+        if($rootScope.currentUser && $rootScope.currentUser.profile){
+            var avatarId = $rootScope.currentUser.profile.avatar;
+            $scope.avatar = $meteor.collectionFS(function(){
+                return Images.find({_id : avatarId});
+            }, false).subscribe('images');
+        }
     }
   };
 });
