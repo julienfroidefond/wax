fed476e9cfcacd80e524ef54b4612018f599482a
Julien FROIDEFOND <jfroidefond@atixnet.fr>
2015-11-10 13:56:15 +0100

    auto add project and chat sample

--- server/startup/users.load.js
+++ server/startup/users.load.js
@@ -13,15 +13,20 @@ Meteor.startup(function() {
         data = JSON.parse(data);
         for(var i in data.users){
             var user = data.users[i];
-
             console.log("-----TRAITEMENT user : "+ user.username);
-
-            addCloudinaryImage(user, function(res){
-
+            addCloudinaryImage(user, function(res, user){
+                if(user.email == 'jfroidefond@atixnet.fr'){
+                    var me = Accounts.findUserByEmail("jfroidefond@atixnet.fr");
+                    createSampleChat(me);
+                    createSampleProject(me);
+                }
             });
-
         }
     }
+    var me = Accounts.findUserByEmail("jfroidefond@atixnet.fr");
+    createSampleChat(me);
+    createSampleProject(me);
+
 
     // if(Meteor.users.find().count() < 12) {
     //     var basePath = process.env.PWD;
@@ -65,17 +70,17 @@ Meteor.startup(function() {
 function addCloudinaryImage(user, callback){
     var basePath = process.env.PWD;
     Cloudinary.api.resource(user.username,
-      Meteor.bindEnvironment(function(result)  {
-          if(result.error && result.error.http_code == 404){
-              Cloudinary.uploader.upload(basePath+'/public/users/'+user.avatar, function(res) {
-                  addUser(user, res.public_id);
-                  return callback(res);
-              }, { public_id: user.username });
-          }else{
-              addUser(user, result.public_id);
-              return callback(result);
-          }
-      })
+        Meteor.bindEnvironment(function(result)  {
+            if(result.error && result.error.http_code == 404){
+                Cloudinary.uploader.upload(basePath+'/public/users/'+user.avatar, function(res) {
+                    addUser(user, res.public_id);
+                    return callback(res, user);
+                }, { public_id: user.username });
+            }else{
+                addUser(user, result.public_id);
+                return callback(result, user);
+            }
+        })
     );
 }
 
@@ -97,6 +102,33 @@ function addUser(user, imageId){
         });
         console.log("WAX ::: Ajout au groupe : "+ user.group);
         Roles.addUsersToRoles(id, user.role, user.group);
+
+    }
+}
+
+function createSampleChat(user){
+    if(Chats.find().count() === 0 && user){
+        console.log("WAX ::: Creation d'un chat exemple");
+        var chat = {
+            "message": "Hello world !",
+            "userId": user._id,
+            "user": user
+        };
+        Chats.insert(chat);
+    }
+}
+
+function createSampleProject(user){
+    if(Projects.find().count() === 0 && user){
+        console.log("WAX ::: Creation d'un projet exemple");
+        var project = {
+            "name": "Projet exemple",
+            "ownerAvatar": user.profile.avatar,
+            "owner": user._id,
+            "description": "<p><span style=\"font-style: italic;\">Ceci<\/span> est <span style=\"font-weight: bold;\">mon<\/span> projet.<\/p><p>Lorem <span style=\"background-color: yellow;\">ipsum<\/span>.<\/p>",
+            "participants": []
+        };
+        Projects.insert(project);
     }
 }
 
