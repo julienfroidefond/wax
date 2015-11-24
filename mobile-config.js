0352bc3e6f879c58a853e90a65f03b8b04e2cfa3
Julien FROIDEFOND <jfroidefond@atixnet.fr>
2015-11-24 14:39:15 +0100

    Test appli Android

---
+++ mobile-config.js
@@ -0,0 +1,12 @@
+
+// the entire section is optional.
+App.info({
+  id: 'com.atixnet.wax',
+  name: 'WAX',
+  description: 'Atixnet WAX',
+  author: 'Julien Froidefond',
+  email: 'jfroidefond@atixnet.fr',
+  website: 'http://atixnet-wax.herokuapp.com/'
+});
+
+App.accessRule("*");
