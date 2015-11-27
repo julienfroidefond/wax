e740d4f4555af1a49973b65516e2fd4395ba4dd6
Julien FROIDEFOND <jfroidefond@atixnet.fr>
2015-11-27 14:09:21 +0100

    Support des mail par mailgun

---
+++ server/startup/mailgun.load.js
@@ -0,0 +1,3 @@
+Meteor.startup( function() {
+  process.env.MAIL_URL = process.env.MAIL_URL = "smtp://postmaster@sandbox4071c121181a409b988e28c1c165a398.mailgun.org:1435a671e587c7a8f7cdb21edd6b2405@smtp.mailgun.org:587";
+});
